0e3b951c6660d23427a58e39d53b3ee8415ad062
Scott Bartell <me@scottbartell.com>
2013-03-20 23:43:53 -0400

    init

---
+++ config/unicorn.rb
@@ -0,0 +1,6 @@
+working_directory "/home/deployer/apps/ing_production/current"
+stderr_path "/home/deployer/apps/ing_production/current/log/unicorn.log"
+pid "/home/deployer/apps/ing_production/current/log/unicorn.pid"
+
+listen "/tmp/unicorn_ing_production.sock"
+worker_processes 1
