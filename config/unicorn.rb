0bf6899f3f899661343cebfe3682680d485a8c19
Scott Bartell <me@scottbartell.com>
2013-03-20 23:51:19 -0400

    fix pid error

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 working_directory "/home/deployer/apps/ing_production/current"
 stderr_path "/home/deployer/apps/ing_production/current/log/unicorn.log"
-pid "/home/deployer/apps/ing_production/current/log/unicorn.pid"
+pid "/home/deployer/apps/ing_production/current/tmp/pids/unicorn.pid"
 
 listen "/tmp/unicorn_ing_production.sock"
 worker_processes 1
