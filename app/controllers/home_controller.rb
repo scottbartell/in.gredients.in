6fa8165a0a6b4b14be42b6832c47a616924f64b3
Scott Bartell <me@scottbartell.com>
2013-04-21 18:33:06 -0400

    make home page work with db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
+  before_action :set_item, only: [:index]
   def index
     if params[:q].present?
-      @recipes = RecipePuppy::Recipe.with_ingredients(params[:q]).get["results"]
+      #@recipes = RecipePuppy::Recipe.with_ingredients(params[:q]).get["results"]
+      @recipes = @item.recipes
     end
   end
+
+  private
+    def set_item
+      @item = Item.find_by name: params[:q]
+      @item = [] unless @item
+    end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+  def title name
+    name.titleize
+  end
+
+  def ingredient_list ingredients
+    ingredients.map{|c| c.name.titleize}.to_sentence
+  end
+
+  def square_image image_name
+    image_name += '_380x250.jpg'
+    image_tag(image_name, size: '380x250')
+  end
 end
