9bd79c45442cbc3134da29a059d692089a8b66b8
Scott Bartell <me@scottbartell.com>
2013-04-21 18:23:35 -0400

    copy item info to ingredient to make db requests easier/faster

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,3 +2,15 @@ class Ingredient < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :item
 end
+  after_initialize :copy_item
+
+  private
+
+  def copy_item
+    item = self.item
+    self.name = item.name
+    self.vegan = item.vegan
+    self.vegetarian = item.vegetarian
+    self.gluten_free = item.gluten_free
+  end
+end
