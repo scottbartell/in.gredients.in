8656fe07f45bd69df861b9e4bd29b710f9513f43
Scott Bartell <me@scottbartell.com>
2013-04-21 18:27:26 -0400

    setup not found error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from NotFoundError, with: :error_not_found
+
+  private
+  
+  def error_not_found
+    render template: 'errors/not_found'
+  end
 end
