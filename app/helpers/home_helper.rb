ead55185de72cdca9f16f6ea840f8c9331e561e6
Scott Bartell <me@scottbartell.com>
2013-04-22 02:28:55 -0400

    update layout and styles

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,11 +4,11 @@ module HomeHelper
   end
 
   def ingredient_list ingredients
-    ingredients.map{|c| c.name.titleize}.to_sentence
+    ingredients.map{|c| c.name.titleize}.to_sentence.truncate(50)
   end
 
   def square_image image_name
-    image_name += '_380x250.jpg'
-    image_tag(image_name, size: '380x250')
+    image_name += '_250x250.jpg'
+    image_tag(image_name, size: '250x250')
   end
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,7 @@
 module RecipesHelper
+  def recipe_image image_name, recipe_title
+    img = image_name
+    img += '_380x250.jpg'
+    image_tag(image_name, size: '380x250', alt: recipe_title.titleize, itemprop: 'image')
+  end
 end
