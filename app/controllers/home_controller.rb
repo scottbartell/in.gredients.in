50e9789de3d1f24e838d7cf988d984a0e04e23f5
Scott Bartell <me@scottbartell.com>
2013-04-22 02:29:00 -0400

    allow multiple items to be search by "like" rather than exact

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,22 @@
 class HomeController < ApplicationController
-  before_action :set_item, only: [:index]
+  before_action :set_items, only: [:index]
   def index
     if params[:q].present?
       #@recipes = RecipePuppy::Recipe.with_ingredients(params[:q]).get["results"]
-      @recipes = @item.recipes
+      @recipes = self.recipes_by_items[0..10]
     end
   end
 
+
+  def recipes_by_items
+    @items.map {|item| item.recipes}.flatten
+  end
+
   private
-    def set_item
-      @item = Item.find_by name: params[:q]
-      @item = [] unless @item
+    def set_items
+      if params[:q].present?
+        @items = Item.where("name like ?", "%#{params[:q].singularize}%")
+        @items = [] unless @items
+      end
     end
 end
