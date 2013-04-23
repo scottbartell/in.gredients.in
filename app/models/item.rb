7b88026d4bf4f46af44f0943c17b098aca456504
Scott Bartell <me@scottbartell.com>
2013-04-23 19:25:12 -0400

    get the top 10 items by count

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,8 @@ class Item < ActiveRecord::Base
     Ingredient.where(item: self).map{|i| i.recipe}
   end
 
+  def self.top_10
+    counted = Item.count('name', group: :name)
+    counted.sort_by {|key, value| value}.reverse[0..10]
+  end
 end
