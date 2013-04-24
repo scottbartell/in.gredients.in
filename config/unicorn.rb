f1d35f7bf5b47ca74448345bfe5f9ca46762b92c
Scott Bartell <me@scottbartell.com>
2013-04-24 03:55:37 -0400

    fix a simple error that took way too long to find

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ working_directory "/home/deployer/apps/ing_production/current"
 stderr_path "/home/deployer/apps/ing_production/current/log/unicorn.log"
 pid "/home/deployer/apps/ing_production/current/tmp/pids/unicorn.pid"
 
-preload_app: true
+preload_app true
 listen "/tmp/unicorn_ing_production.sock"
 worker_processes 1
